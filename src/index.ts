7fb0ad30c4d1d44c2d24178d3e96b7cfb68ee349
cosmos-explorer <lian.yang.work@gmail.com>
2022-11-23 22:54:09 +0100

    fix: escape ampersand

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,17 @@ import { XMLParser, XMLBuilder } from 'fast-xml-parser';
 import { promises as fs }  from 'fs';
 import path from 'path';
 
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
 export const generate = async (locales: string[], basePath: string, logger: any) => {
+  const randomStr = `#${uuidv4()}#`;
+  const randomStrRegex = new RegExp(randomStr, "g");
+
   const attributeNamePrefix = "@_";
   const options = {
     ignoreAttributes: false,
@@ -13,7 +23,17 @@ export const generate = async (locales: string[], basePath: string, logger: any)
   const builder = new XMLBuilder(options);
   // with stopNodes option, the source tag's value would not be parsed
   // so value like <source>Hi, <x id="INTERPOLATION" equiv-text="{{ name }}"/>! LL</source> would be kept in the ouput
-  const parser = new XMLParser({...options, stopNodes: ["xliff.file.body.trans-unit.source", "xliff.file.body.trans-unit.target"]});
+  // tagValueProcessor replace all & by a random string which will be replaced back to & at the end of the workflow
+  const parser = new XMLParser({
+    ...options,
+    stopNodes: ["xliff.file.body.trans-unit.source", "xliff.file.body.trans-unit.target"],
+    tagValueProcessor: (tagName: string, tagValue: string, jPath: string, hasAttributes: boolean, isLeafNode: boolean) => {
+      if (tagName === 'source' || tagName === 'target') {
+        return tagValue.replace(/&/g, randomStr);
+      }
+
+      return tagValue;
+  }});
 
   const sourceXlf = await fs.readFile(path.join(basePath, 'messages.xlf'), { encoding: 'utf8' });
   const sourceObjecct = parser.parse(sourceXlf);
@@ -44,10 +64,13 @@ export const generate = async (locales: string[], basePath: string, logger: any)
     const translatedObject = { ...sourceObjecct };
     translatedObject.xliff.file[`${attributeNamePrefix}source-language`] = locale;
     translatedObject.xliff.file.body['trans-unit'] = translatedUnits;
+
+
     const translatedXlf = builder.build(translatedObject)
         .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
         .replace(/&quot;/g, '"')
-        .replace(/&gt;/g, '>');
+        .replace(randomStrRegex, '&');
 
     await fs.writeFile(path.join(basePath, `messages.${locale}.xlf`), translatedXlf);
     logger.log({ level: 'info', message: `Done messages.${locale}.xlf`});
